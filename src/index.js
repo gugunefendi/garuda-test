7c70c0bd37a132d643dca6569425564663748484
Gunawan Efendi <gunawanefendi32@gmail.com>
2025-04-29 15:06:29 +0700

    Modular Routing

--- src/index.js
+++ src/index.js
@@ -1,76 +1,21 @@
 import express from 'express'
 const app = express()
 const port = 3000
-import url from 'url'
+import router from './page/routes/index.js'
 
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
-
-
-const cb0 = (req, res, next) => {
-    console.log('CB0')
-    next()
+const authenticated = (req, res, next) => {
+    if (true) {
+        next()
+    }
 }
 
-const cb1 = (req, res, next) => {
-    console.log('CB1')
-    next()
+const validated = (req, res, next) => {
+    if (true) {
+        next()
+    }
 }
 
-const cb2 = (req, res, next) => {
-    console.log('CB2')
-    next()
-}
-
-app.get('/products2', [cb0, cb1, cb2], (req, res) => {
-    res.json([
-        'Route handler'
-    ])
-})
-
-// simple respond
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-// respond file
-app.get('/product', (req, res) => {
-    res.sendFile('./page/products.html', {root: __dirname})
-})
-
-// response json
-app.get('/jsonresponse', (req, res) => {
-    res.json({
-        id: 1,
-        name: 'Gunawan'
-    })
-})
-
-// respond file
-app.get('/home', (req, res) => {
-    res.sendFile('./page/index.html', {root: __dirname})
-})
-
-// get request from url
-app.get('/product/:id', (req, res) => {
-    console.log(req.params)
-    const id = req.params.id
-    res.send('Product page with product id: ' + id)
-})
-
-// get request from url with query
-app.get('/cart/:id', (req, res) => {
-    console.log(req)
-    const cartId = req.params.id
-    const productName = req.query.product_name
-    res.send('Cart page with cart id: ' + cartId + ' and product name: ' + productName)
-})
-
-// default page if any page not found
-app.use('/', (req, res) => {
-    res.status(404)
-    // res.send("Page not found")
-    res.sendFile('./page/404.html', {root: __dirname})
-})
+app.use([authenticated, validated], router)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- src/page/routes/product.js
+++ src/page/routes/product.js
@@ -1,22 +1,35 @@
-import express from 'express'
+import express from "express"
+const productRouter =  express.Router()
 
-const cb0 = (req, res) => {
-    console.log('CB0')
-    next()
-}
+productRouter
+    .route('/')
+    .get((req, res) => {
+        res.json({
+            message: 'GET all products'
+        })
+    })
+    .post((req, res) => {
+        res.json({
+            message: 'POST product'
+        })
+    })
 
-const cb1 = (req, res) => {
-    console.log('CB1')
-    next()
-}
+productRouter
+    .route('/:id')
+    .get((req, res) => {
+        res.json({
+            message: 'GET product with ID: ' + req.params.id
+        })
+    })
+    .put((req, res) => {
+        res.json({
+            message: 'UPDATE product ID: ' + req.params.id
+        })
+    })
+    .delete((req, res) => {
+        res.json({
+            message: 'DELETE product ID: ' + req.params.id
+        })
+    })
 
-const cb2 = (req, res) => {
-    console.log('CB2')
-    next()
-}
-
-app.get('/products2', [cb0, cb1, cb2], (req, res) => {
-    res.json([
-        'Route handler'
-    ])
-})
+export default productRouter
